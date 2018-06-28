2ff6c8b1fd3fa93e2c83831f640230de38b67353
Stefano Magni <stefano@creeostudio.it>
2018-06-28 09:01:26 +0200

    file rename

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const extension = require('./src/index.js');
+const extension = require('./src/increase-by-one.js');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
