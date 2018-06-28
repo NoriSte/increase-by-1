9ef0f0d5a34f4b4151a62c774cb1a880e03c68e3
Stefano Magni <stefano@creeostudio.it>
2018-06-28 13:46:54 +0200

    command rename

--- extension.js
+++ extension.js
@@ -14,9 +14,8 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.increaseByOne', function () {
+    let disposable = vscode.commands.registerCommand('increaseByOne.integers', function () {
         // The code you place here will be executed every time your command is executed
-        console.log(extension);
         extension(vscode);
     });
 
