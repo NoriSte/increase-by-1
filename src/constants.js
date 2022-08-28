1e1913c2ecd4565e0deea8e13950e6fa80be2aae
Aziz <abodenaal@gmail.com>
2022-08-28 19:29:38 +0300

    make error message more helpful
    I updated the error message that said "Select at least two occurencies" to say "Place cursors at two or more numbers" because it took me a while to understand that that's what I was supposed to do. Also, added missing apostrophe for NO_SELECTIONS message.

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
     NO_EDITOR: 'There isn\'t any opened editor',
-    NO_SELECTIONS: 'There isn\t any selection',
-    NOT_ENOUGH_SELECTIONS: 'Select at least two occurrencies',
+    NO_SELECTIONS: 'There isn\'t any selection',
+    NOT_ENOUGH_SELECTIONS: 'Place cursors at two or more numbers',
     NOT_FIRST_SELECTION_NUMBER: 'The first selection must be a number'
 };
